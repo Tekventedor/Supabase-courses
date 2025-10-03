2061b06caf0df6066c6fc08572ebdbebc5a2ea86
Hugo Lewis Plant <hugolewisplant@Hugos-MacBook-Air-2.local>
2025-10-03 15:52:44 +0200

    fix: add @types/bcryptjs and correct outputFileTracingRoot location

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    outputFileTracingRoot: __dirname,
-  },
+  outputFileTracingRoot: __dirname,
 };
 
 module.exports = nextConfig;
